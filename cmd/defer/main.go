7542f29a24f471a213f7f2fd3d994b6f654e2e49
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-09 09:04:01 +0630

    miscellaneous explorations

--- cmd/defer/main.go
+++ cmd/defer/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	// "io"
-	// "os"
+	"io"
+	"os"
 	"strings"
 )
 
 func main() {
 	url := "https://github.com"
 	protocol, domain := urlCutter(url)
-	// createAndWrite("test", protocol, domain)
+	createAndWrite("test", protocol, domain)
 }
 
 func urlCutter(url string) (string, string) {
@@ -19,20 +19,20 @@ func urlCutter(url string) (string, string) {
 	return "protocol: " + protocol, "domain: " + domain
 }
 
-// func createAndWrite(filename string, text ...string) error {
-// 	stringToWrite := strings.Join(text, "\n")
-// 	filepath := "static/" + filename + ".txt"
-// 	fmt.Println(filepath)
-//
-// 	var err error
-// 	filePointer, err := os.Create(filepath)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_, err = io.WriteString(filePointer, stringToWrite)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer filePointer.Close()
-// 	return filePointer.Close()
-// }
+func createAndWrite(filename string, text ...string) error {
+	stringToWrite := strings.Join(text, "\n")
+	filepath := "static/" + filename + ".txt"
+	fmt.Println(filepath)
+
+	var err error
+	filePointer, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(filePointer, stringToWrite)
+	if err != nil {
+		return err
+	}
+	defer filePointer.Close()
+	return filePointer.Close()
+}
